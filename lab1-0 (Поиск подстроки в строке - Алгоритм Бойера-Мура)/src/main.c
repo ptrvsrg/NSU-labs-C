ac6bf1722e3f2ac4d067b9e4d5cf24f1e1d48816
ptrvsrg <s.petrov1@g.nsu.ru>
2022-04-22 23:39:35 +0700

    Update main.c

--- lab1-0 (Поиск подстроки в строке - Алгоритм Бойера-Мура)/src/main.c
+++ lab1-0 (Поиск подстроки в строке - Алгоритм Бойера-Мура)/src/main.c
@@ -1,131 +1,126 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef unsigned char uchar;
+#define UNUSED(x) (void)(x);
 
 enum
 {
-	SIZE_TEMPLATE = 16,
+    SIZE_TEMPLATE = 16,
     SIZE_TEXT = 2 * SIZE_TEMPLATE,
-	SIZE_CHAR = 256
+    SIZE_CHAR = 256
 };
 
 void PrintNumber(int number)
 {
-	printf("%u ", number);
+    printf("%d ", number);
 }
 
-void InputTemplate(uchar* template)
+void InputTemplate(char* template)
 {
     for (int i = 0; i < SIZE_TEMPLATE + 1; ++i)
-	{
-		int control = scanf("%c", &template[i]);
-		assert(control == 1);
+    {
+        int control = scanf("%c", &template[i]);
+        UNUSED(control);
+        assert(control == 1);
 
-		if (template[i] == '\n')
-		{
-			template[i] = '\0';
-			return;
-		}
-	}
+        if (template[i] == '\n')
+        {
+            template[i] = '\0';
+            return;
+        }
+    }
 
-	assert(false);
+    assert(false);
 }
 
-void CreateShiftTable(const uchar* template, int shiftTable[])
+void CreateShiftTable(const char* template, int shiftTable[])
 {
-	const int templateLength = strlen(template);
+    const int templateLength = strlen(template);
 
-	for (int i = 0; i < SIZE_CHAR; ++i)
-	{
-		shiftTable[i] = templateLength;
-	}
+    for (int i = 0; i < SIZE_CHAR; ++i)
+    {
+        shiftTable[i] = templateLength;
+    }
 
-	for (int i = 1; i < templateLength; ++i)
-	{
+    for (int i = 1; i < templateLength; ++i)
+    {
         int index = template[templateLength - 1 - i];
 
-		if (shiftTable[index] == templateLength)
-		{
-			shiftTable[index] = i;
-		}
-	}
-}
-
-void StrCpy(uchar* strDest, const uchar* strSrc)
-{
-    assert(strDest != NULL && strSrc != NULL);
-    uchar *temp = strDest;
-
-    while(*strDest)
-    {
-        *strDest = *strSrc;
-        ++strDest;
-        ++strSrc;
+        if (shiftTable[index] == templateLength)
+        {
+            shiftTable[index] = i;
+        }
     }
 }
 
-void ShiftText(uchar* text)
+int ShiftText(char* text)
 {
-    StrCpy(text, text + SIZE_TEMPLATE);
-    int control = fread(text + SIZE_TEMPLATE, sizeof(uchar), SIZE_TEMPLATE, stdin);
+    for (int i = 0; i < SIZE_TEMPLATE; ++i)
+    {
+        text[i] = text[i + SIZE_TEMPLATE];
+    }
+        
+    int control = fread(text + SIZE_TEMPLATE, sizeof(char), SIZE_TEMPLATE, stdin);
+    UNUSED(control);
     assert(control != EOF);
+
+    return control;
 }
 
-void BoyerMooreAlgorithm(const uchar* template)
+void BoyerMooreAlgorithm(const char* template)
 {
-	const int templateLength = strlen(template);
+    const int templateLength = strlen(template);
     int currentPosition = templateLength;
     int comparisonPosition = templateLength;
     int lastLength = 0;
-    
-	int shiftTable[SIZE_CHAR];
-	CreateShiftTable(template, shiftTable);
+        
+    int shiftTable[SIZE_CHAR] = { 0 };
+    CreateShiftTable(template, shiftTable);
 
-    uchar text[SIZE_TEXT + 1] = {0};
-    int control = fread(text, sizeof(uchar), SIZE_TEXT, stdin);
+    char text[SIZE_TEXT + 1] = { 0 };
+    int control = fread(text, sizeof(char), SIZE_TEXT, stdin);
+    UNUSED(control);
     assert(control != EOF);
+    int textLength = strlen(text);
 
     while (true)
-	{
-        if (comparisonPosition > lastLength + strlen(text))
+    {
+        if ((unsigned int)comparisonPosition > (unsigned int)lastLength + textLength)
         {
-            lastLength += SIZE_TEMPLATE;
-            ShiftText(text);
-
-            if (strlen(text) <= SIZE_TEMPLATE)
+            if (ShiftText(text) == 0)
             {
                 return;
             }
+
+            lastLength += SIZE_TEMPLATE;
+            textLength = SIZE_TEXT;
         }
 
-		for (int i = 0; i < templateLength; ++i)
-		{
-			PrintNumber(currentPosition);
+        for (int i = 0; i < templateLength; ++i)
+        {
+            PrintNumber(currentPosition);
 
-			if (text[comparisonPosition - lastLength - 1 - i] != template[templateLength - 1 - i] || i == templateLength - 1)
-			{				
-				comparisonPosition += shiftTable[text[comparisonPosition - lastLength - 1]];
-				currentPosition = comparisonPosition;
-				break;
-			}
-			
+            if (text[comparisonPosition - lastLength - 1 - i] != template[templateLength - 1 - i] || i == templateLength - 1)
+            {               
+                comparisonPosition += shiftTable[(unsigned char)text[comparisonPosition - lastLength - 1]];
+                currentPosition = comparisonPosition;
+                break;
+            }
+            
             --currentPosition;
-		}
-	}
+        }
+    }
 
 }
 
-int main()
+int main(void)
 {
-	char template[SIZE_TEMPLATE + 1];
+    char template[SIZE_TEMPLATE + 1] = { 0 };
 
-	InputTemplate(template);
-	BoyerMooreAlgorithm(template);
+    InputTemplate(template);
+    BoyerMooreAlgorithm(template);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
--- lab1-1 (Поиск подстроки в строке - Алгоритм Рабина-Карпа)/src/main.c
+++ lab1-1 (Поиск подстроки в строке - Алгоритм Рабина-Карпа)/src/main.c
@@ -2,83 +2,81 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 enum
 {
-	SUCCESS = 0,
-	SIZE_TEMPLATE = 16
+    SIZE_TEMPLATE = 16
 };
 
 void PrintNumber(int Number)
 {
-	printf("%d ", Number);
+    printf("%d ", Number);
 }
 
-void InputTemplate(char* Template, FILE* InputFile)
+void InputTemplate(char* Template, FILE* inFile)
 {
     for (int i = 0; i < SIZE_TEMPLATE + 1; i++)
-	{
-		if ((Template[i] = (char)fgetc(InputFile)) == EOF)
-		{
-			fclose(InputFile);
-			assert(false);
-		}
+    {
+        if ((Template[i] = (char)fgetc(inFile)) == EOF)
+        {
+            assert(false);
+        }
 
-		if (Template[i] == '\n')
-		{
-			if (i == 0)
-			{
-				fclose(InputFile);
-				assert(false);
-			}
-			else
-			{
-				Template[i] = '\0';
-				break;
-			}
-		}
-	}
+        if (Template[i] == '\n')
+        {
+            if (i == 0)
+            {
+                assert(false);
+            }
+            else
+            {
+                Template[i] = '\0';
+                break;
+            }
+        }
+    }
 }
 
 int Pow(unsigned int Number, int Power)
 {
     assert(Power >= 0);
 
-	if (Number == 0 || Number == 1)
-	{
-		return Number;
-	}
+    if (Number == 0 || Number == 1)
+    {
+        return Number;
+    }
 
-	if (Power == 0)
-	{
-		return 1;
-	}
-	else
-	{
-		return Number * Pow(Number, Power - 1);
-	}
+    if (Power == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return Number * Pow(Number, Power - 1);
+    }
 }
 
 int HashFunction(const char* Line)
 {
-	int Sum = 0;
-	int Power = 1;
-	int LineLength = strlen(Line);
+    int Sum = 0;
+    int Power = 1;
+    int LineLength = strlen(Line);
 
-	for (int i = 0; i < LineLength; i++)
-	{
-		Sum += (unsigned char)Line[i] % 3 * Power;
-		Power *= 3;
-	}
+    for (int i = 0; i < LineLength; i++)
+    {
+        Sum += (unsigned char)Line[i] % 3 * Power;
+        Power *= 3;
+    }
 
-	return Sum;
+    return Sum;
 }
 
-void ShiftText(int TemplateLength, char* Text, FILE* InputFile)
+void ShiftText(int TemplateLength, char* Text, FILE* inFile)
 {
     memmove(Text, Text + 1, TemplateLength);
-	Text[TemplateLength - 1] = (char)fgetc(InputFile);
+    Text[TemplateLength - 1] = (char)fgetc(inFile);
 }
 
 void ChangeHash(unsigned char Symbol1, unsigned char Symbol2, int PowerOf3, int* TextHash)
@@ -88,58 +86,59 @@ void ChangeHash(unsigned char Symbol1, unsigned char Symbol2, int PowerOf3, int*
     *TextHash += (unsigned char)Symbol2 % 3 * PowerOf3;
 }
 
-void RabinKarpAlgorithm(const char* Template, FILE* InputFile)
+void RabinKarpAlgorithm(const char* Template, FILE* inFile)
 {
-	const int TemplateLength = strlen(Template);
-	const int TemplateHash = HashFunction(Template);
-	PrintNumber(TemplateHash);
+    const int TemplateLength = strlen(Template);
+    const int TemplateHash = HashFunction(Template);
+    PrintNumber(TemplateHash);
 
-	char Text[SIZE_TEMPLATE + 1];
-	int Position = 1;
-	memset(Text, 0, SIZE_TEMPLATE + 1);
-	const int PowerOf3 = Pow(3, TemplateLength - 1);
+    char Text[SIZE_TEMPLATE + 1];
+    int Position = 1;
+    memset(Text, 0, SIZE_TEMPLATE + 1);
+    const int PowerOf3 = Pow(3, TemplateLength - 1);
 
-	if (fread(Text, 1, TemplateLength, InputFile) != (size_t)TemplateLength)
-	{
-		return;
-	}
+    if (fread(Text, 1, TemplateLength, inFile) != (size_t)TemplateLength)
+    {
+        return;
+    }
 
-	int TextHash = HashFunction(Text);
+    int TextHash = HashFunction(Text);
 
-	while (!feof(InputFile))
-	{
-		if (TemplateHash == TextHash)
-		{
-			for (int i = 0; i < TemplateLength; i++)
-			{
-				PrintNumber(Position + i);
+    while (!feof(inFile))
+    {
+        if (TemplateHash == TextHash)
+        {
+            for (int i = 0; i < TemplateLength; i++)
+            {
+                PrintNumber(Position + i);
 
-				if (Template[i] != Text[i])
-				{
-					break;
-				}
-			}
-		}
+                if (Template[i] != Text[i])
+                {
+                    break;
+                }
+            }
+        }
 
-		unsigned char Symbol = Text[0];
+        unsigned char Symbol = Text[0];
 
-		ShiftText(TemplateLength, Text, InputFile);
-		ChangeHash(Symbol, Text[TemplateLength - 1], PowerOf3, &TextHash);
+        ShiftText(TemplateLength, Text, inFile);
+        ChangeHash(Symbol, Text[TemplateLength - 1], PowerOf3, &TextHash);
 
-		++Position;	
-	}
+        ++Position; 
+    }
 }
 
 int main(void)
 {
-	char Template[SIZE_TEMPLATE + 1];
-	memset(Template, 0, SIZE_TEMPLATE + 1);
-	FILE* InputFile = fopen("in.txt", "r");
+    char Template[SIZE_TEMPLATE + 1] = { 0 };
 
-	InputTemplate(Template, InputFile);
+    FILE* inFile = fopen("in.txt", "r");
+    assert(inFile != NULL);
 
-	RabinKarpAlgorithm(Template, InputFile);
-	fclose(InputFile);
+    InputTemplate(Template, inFile);
+    RabinKarpAlgorithm(Template, inFile);
 
-	return SUCCESS;
+    fclose(inFile);
+
+    return EXIT_SUCCESS;
 }
