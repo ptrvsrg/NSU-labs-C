b1275ab4b566c5974b6e70dfbf639fe1f4559174
s.petrov1 <s.petrov1@g.nsu.ru>
2022-07-30 23:06:19 +0800

    Update asan

--- modules/enable_sanitizers.cmake
+++ modules/enable_sanitizers.cmake
@@ -1,8 +1,7 @@
 if(WIN32)
 	if(ENABLE_ASAN)
 		target_compile_options(${PROJECT_NAME} PUBLIC -fsanitize=address)
-		target_link_libraries(${PROJECT_NAME} "C:/Program Files/LLVM/lib/clang/7.0.0/lib/windows/clang_rt.asan-x86_64.lib")
-		target_compile_options(${PROJECT_NAME} PUBLIC /MT)
+        target_compile_options(${PROJECT_NAME} PUBLIC /MT)
 	endif()
 
 	if(ENABLE_USAN)
