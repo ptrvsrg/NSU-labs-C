a1d809a33881873cff8591fe124838d725c775d1
s.petrov1 <s.petrov1@g.nsu.ru>
2022-07-30 20:40:56 +0800

    Update PowerShell script

--- test.ps1
+++ test.ps1
@@ -56,7 +56,7 @@ Remove-Item $BUILD_TEST_DIR -Force -Recurse 2>&1 > $null
 Write-Host -ForegroundColor Yellow "TEST 3"
 New-Item -Path $BUILD_TEST_DIR -ItemType "directory" 2>&1 > $null
 Set-Location -Path $BUILD_TEST_DIR
-cmake ../$LAB -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON
+cmake ../$LAB -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64/cl.exe" -DENABLE_ASAN=true -DUNLIMITED=ON
 cmake --build . --config Debug
 ctest -C Debug --rerun-failed --output-on-failure
 if (Test-Path ./Testing/Temporary/LastTestsFailed.log) 
@@ -70,7 +70,7 @@ Remove-Item $BUILD_TEST_DIR -Force -Recurse 2>&1 > $null
 Write-Host -ForegroundColor Yellow "TEST 4"
 New-Item -Path $BUILD_TEST_DIR -ItemType "directory" 2>&1 > $null
 Set-Location -Path $BUILD_TEST_DIR
-cmake ../$LAB -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON
+cmake ../$LAB -DCMAKE_C_COMPILER="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64/cl.exe" -DENABLE_USAN=true -DUNLIMITED=ON
 cmake --build . --config Debug
 ctest -C Debug --rerun-failed --output-on-failure
 if (Test-Path ./Testing/Temporary/LastTestsFailed.log) 
