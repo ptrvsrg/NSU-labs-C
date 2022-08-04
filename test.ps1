ae197f40efbfc25437860f72cdfcb940a720a01e
s.petrov1 <s.petrov1@g.nsu.ru>
2022-08-04 16:55:48 +0800

    Add script param

--- test.ps1
+++ test.ps1
@@ -1,9 +1,13 @@
-if($args[0])
-{
-    $LAB = $args[0]
-    $BUILD_TEST_DIR = "build_$LAB"
+Param (
+    [string]$Lab,
+    [string]$Test
+)
 
-    if (-not (Test-Path -Path $LAB))
+if($Lab)
+{
+    $BUILD_TEST_DIR = "build_$Lab"
+
+    if (-not (Test-Path -Path $Lab))
     {
         Write-Host -ForegroundColor Red "No such file or directory"
         exit 1
@@ -11,7 +15,7 @@ if($args[0])
 }
 else 
 {
-    $LAB = "."
+    $Lab = "."
     $BUILD_TEST_DIR = "build_all_labs"
 }
 
@@ -22,25 +26,37 @@ if (Test-Path -Path $BUILD_TEST_DIR)
 
 New-Item -Path $BUILD_TEST_DIR -ItemType "directory" 2>&1 > $null
 
-Write-Host -ForegroundColor Yellow "TEST 1"
-cmake -B $BUILD_TEST_DIR/Test1 -S $LAB -DUNLIMITED=ON
-cmake --build $BUILD_TEST_DIR/Test1 --config Debug
-if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test1 --target RUN_TESTS}
-if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test1 --target test}
+if ((-not $Test) -or ($Test).Contains('Test1'))
+{
+    Write-Host -ForegroundColor Yellow "TEST 1"
+    cmake -B $BUILD_TEST_DIR/Test1 -S $Lab -DUNLIMITED=ON
+    cmake --build $BUILD_TEST_DIR/Test1 --config Debug
+    if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test1 --target RUN_TESTS}
+    if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test1 --target test}
+}
 
-Write-Host -ForegroundColor Yellow "TEST 2"
-cmake -B $BUILD_TEST_DIR/Test2 -S $LAB -DUNLIMITED=OFF
-cmake --build $BUILD_TEST_DIR/Test2 --config Debug
-if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test2 --target RUN_TESTS}
-if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test2 --target test}
+if ((-not $Test) -or ($Test).Contains('Test2'))
+{
+    Write-Host -ForegroundColor Yellow "TEST 2"
+    cmake -B $BUILD_TEST_DIR/Test2 -S $Lab -DUNLIMITED=OFF
+    cmake --build $BUILD_TEST_DIR/Test2 --config Debug
+    if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test2 --target RUN_TESTS}
+    if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test2 --target test}
+}
 
-Write-Host -ForegroundColor Yellow "TEST 3"
-cmake -B $BUILD_TEST_DIR/Test3 -S $LAB -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON
-if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test3 --target RUN_TESTS}
-if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test3 --target test}
+if ((-not $Test) -or ($Test).Contains('Test3'))
+{
+    Write-Host -ForegroundColor Yellow "TEST 3"
+    cmake -B $BUILD_TEST_DIR/Test3 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON
+    if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test3 --target RUN_TESTS}
+    if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test3 --target test}
+}
 
-Write-Host -ForegroundColor Yellow "TEST 4"
-cmake -B $BUILD_TEST_DIR/Test4 -S $LAB -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON
-cmake --build $BUILD_TEST_DIR/Test4 --config Debug
-if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test4 --target RUN_TESTS}
-if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test4 --target test}
+if ((-not $Test) -or ($Test).Contains('Test4'))
+{
+    Write-Host -ForegroundColor Yellow "TEST 4"
+    cmake -B $BUILD_TEST_DIR/Test4 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON
+    cmake --build $BUILD_TEST_DIR/Test4 --config Debug
+    if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test4 --target RUN_TESTS}
+    if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test4 --target test}
+}
