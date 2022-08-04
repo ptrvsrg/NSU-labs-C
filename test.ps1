fa73191eab308bb32bc52ac2f1ef2cdb53eb4eef
s.petrov1 <s.petrov1@g.nsu.ru>
2022-08-04 10:02:10 +0800

    Remove exit

--- test.ps1
+++ test.ps1
@@ -26,36 +26,18 @@ Write-Host -ForegroundColor Yellow "TEST 1"
 cmake -B $BUILD_TEST_DIR/Test1 -S $LAB -DUNLIMITED=ON
 cmake --build $BUILD_TEST_DIR/Test1 --config Debug
 cmake --build $BUILD_TEST_DIR/Test1 --target RUN_TESTS
-if (Test-Path $BUILD_TEST_DIR/Testing/Temporary/LastTestsFailed.log) 
-{ 
-    exit 1 
-}
 
 Write-Host -ForegroundColor Yellow "TEST 2"
 cmake -B $BUILD_TEST_DIR/Test2 -S $LAB -DUNLIMITED=OFF
 cmake --build $BUILD_TEST_DIR/Test2 --config Debug
 cmake --build $BUILD_TEST_DIR/Test2 --target RUN_TESTS
-if (Test-Path $BUILD_TEST_DIR/Testing/Temporary/LastTestsFailed.log) 
-{ 
-    exit 1 
-}
 
 Write-Host -ForegroundColor Yellow "TEST 3"
 cmake -B $BUILD_TEST_DIR/Test3 -S $LAB -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON
 cmake --build $BUILD_TEST_DIR/Test3 --config Debug
 cmake --build $BUILD_TEST_DIR/Test3 --target RUN_TESTS
-if (Test-Path $BUILD_TEST_DIR/Testing/Temporary/LastTestsFailed.log) 
-{ 
-    exit 1 
-}
 
 Write-Host -ForegroundColor Yellow "TEST 4"
 cmake -B $BUILD_TEST_DIR/Test4 -S $LAB -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON
 cmake --build $BUILD_TEST_DIR/Test4 --config Debug
 cmake --build $BUILD_TEST_DIR/Test4 --target RUN_TESTS
-if (Test-Path $BUILD_TEST_DIR/Testing/Temporary/LastTestsFailed.log) 
-{ 
-    exit 1 
-}
-
-exit 0
