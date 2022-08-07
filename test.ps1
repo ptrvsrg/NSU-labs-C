3e3d7420d170d4a9b7546a703b2a0c42c63de3f2
s.petrov1 <s.petrov1@g.nsu.ru>
2022-08-08 01:08:15 +0800

    Fix build config

--- test.ps1
+++ test.ps1
@@ -26,38 +26,38 @@ if (Test-Path -Path $BUILD_TEST_DIR)
 
 New-Item -Path $BUILD_TEST_DIR -ItemType "directory" 2>&1 > $null
 
-if ((-not $Test) -or (($Test).ToLower()).Contains('Test1'))
+if ((-not $Test) -or ($Test).Contains('Test1'))
 {
     Write-Host -ForegroundColor Yellow "TEST 1"
-    cmake -B $BUILD_TEST_DIR/Test1 -S $Lab -DUNLIMITED=ON
+    cmake -B $BUILD_TEST_DIR/Test1 -S $Lab -DUNLIMITED=ON -DCMAKE_BUILD_TYPE=Release
     cmake --build $BUILD_TEST_DIR/Test1 --config Release
     if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test1 --target RUN_TESTS}
     if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test1 --target test}
 }
 
-if ((-not $Test) -or (($Test).ToLower()).Contains('Test2'))
+if ((-not $Test) -or ($Test).Contains('Test2'))
 {
     Write-Host -ForegroundColor Yellow "TEST 2"
-    cmake -B $BUILD_TEST_DIR/Test2 -S $Lab -DUNLIMITED=OFF
+    cmake -B $BUILD_TEST_DIR/Test2 -S $Lab -DUNLIMITED=OFF -DCMAKE_BUILD_TYPE=Release
     cmake --build $BUILD_TEST_DIR/Test2 --config Release
     if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test2 --target RUN_TESTS}
     if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test2 --target test}
 }
 
-if ((-not $Test) -or (($Test).ToLower()).Contains('Test3'))
+if ((-not $Test) -or ($Test).Contains('Test3'))
 {
     Write-Host -ForegroundColor Yellow "TEST 3"
-    cmake -B $BUILD_TEST_DIR/Test3 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON
-    cmake --build $BUILD_TEST_DIR/Test3
+    cmake -B $BUILD_TEST_DIR/Test3 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_ASAN=true -DUNLIMITED=ON  -DCMAKE_BUILD_TYPE=Debug
+    cmake --build $BUILD_TEST_DIR/Test3 --config Debug
     if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test3 --target RUN_TESTS}
     if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test3 --target test}
 }
 
-if ((-not $Test) -or (($Test).ToLower()).Contains('Test4'))
+if ((-not $Test) -or ($Test).Contains('Test4'))
 {
     Write-Host -ForegroundColor Yellow "TEST 4"
-    cmake -B $BUILD_TEST_DIR/Test4 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON
-    cmake --build $BUILD_TEST_DIR/Test4
+    cmake -B $BUILD_TEST_DIR/Test4 -S $Lab -DCMAKE_C_COMPILER=clang -DENABLE_USAN=true -DUNLIMITED=ON -DCMAKE_BUILD_TYPE=Debug
+    cmake --build $BUILD_TEST_DIR/Test4 --config Debug
     if ($IsWindows) {cmake --build $BUILD_TEST_DIR/Test4 --target RUN_TESTS}
     if ($IsLinux) {cmake --build $BUILD_TEST_DIR/Test4 --target test}
 }
