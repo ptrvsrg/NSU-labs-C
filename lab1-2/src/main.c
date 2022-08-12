6f2e57a96b3e9006b43112e8c266b71e1080e8f9
s.petrov1 <s.petrov1@g.nsu.ru>
2022-08-12 22:38:35 +0800

    Remove freopen

--- lab1-2/src/main.c
+++ lab1-2/src/main.c
@@ -6,15 +6,9 @@
 
 int main(void)
 {
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-
     TString sample = CreateString();
     InputSample(&sample);
     KMPAlgorithm(sample);
-
-    fclose(stdin);
-    fclose(stdout);
 
     return EXIT_SUCCESS;
 }
